450aad12b1019ba4b230ecd7f730ee98fee82164
adocxwork <adipracc@gmail.com>
2023-08-06 12:03:25 +0530

    files added..

--- Assignments/a5/a8.java
+++ Assignments/a5/a8.java
@@ -1,14 +1,50 @@
+// Write a program to print a triangle of prime numbers upto given number of lines of the trinagle.
 public class a8 {
-    public static void main(String[] args) {
-        // Write a program to print a triangle of prime numbers upto given number of lines of the trinagle.
-        int n = 11;
-        for(int i=1; i<=11; i++)
+    static int primeNum(int indexNo)
+    {
+        int tempIndex = indexNo;
+        boolean isPrime = false;
+        int x=2;
+        while(tempIndex>0)
         {
-            for(int j=n; j>=1+i; j--)
+            for(int i=2; i<=x; i++)
+            {
+                if(x%i==0 && i!=x)
+                {
+                    isPrime=false;
+                    x++;
+                    break;
+                }
+                else
+                {
+                    isPrime=true;
+                }
+            }
+            if(isPrime==true)
+            {
+                tempIndex--;
+                isPrime=false;
+                x++;
+            }
+        }
+        return (--x);
+    }
+    public static void main(String[] args) {
+        int n=4;
+        int tempK=1;
+        for(int i=1; i<=n; i++)
+        {
+            // spaces
+            for(int j=1; j<=n-i; j++)
             {
                 System.out.print(" ");
             }
-            System.out.print("hi");
+            for(int k=1; k<=i; k++)
+            {
+                System.out.print(primeNum(tempK) + " ");
+                tempK++;
+            }
+            
             System.out.println();
         }
     }
