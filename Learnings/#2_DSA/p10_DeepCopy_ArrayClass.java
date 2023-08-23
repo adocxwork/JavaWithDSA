c7051a23a4e42357a0c54573720463385dd4cb3c
adocxwork <adipracc@gmail.com>
2023-08-23 14:16:29 +0530

    files added..

--- Learnings/#2_DSA/p10_DeepCopy_ArrayClass.java
+++ Learnings/#2_DSA/p10_DeepCopy_ArrayClass.java
@@ -1,16 +1,31 @@
 import java.util.Arrays;
 
 public class p10_DeepCopy_ArrayClass {
+
+    static void displayArray(int arr[])
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         int arr[] = {4,5,6,7,8};
-        // int arr2[] = Arrays.copyOf(arr, 2);
-        int arr2[] = Arrays.copyOf(arr, arr.length);
 
-        arr2[0] = 0; //this is a separate copy
+        // int arr2[] = Arrays.copyOf(arr, arr.length); //this will clone the whole array
+        int arr2[] = Arrays.copyOf(arr, 2); //this will only copy first 2 elements
+        int arr3[] = Arrays.copyOfRange(arr, 2, 4);
+        
+        System.out.println("Original Array :-");
+        displayArray(arr);
+        System.out.println("Cloned Array - copyOf :-");
+        displayArray(arr2);
 
-        for(int i=0; i<arr2.length; i++)
-        {
-            System.out.print(arr2[i] + " ");
-        }
+        System.out.println("Cloned Array - copyOfRange :-");
+        displayArray(arr3);
+        
+
     }
 }
