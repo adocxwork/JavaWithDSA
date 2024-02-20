44bda0f3194bbaf10777a930738ea728dde66f37
adocxwork <adipracc@gmail.com>
2024-02-20 13:12:53 +0530

    files added..

---
+++ Learnings/#2_DSA/#7_Strings/p1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class p1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String str = sc.next();
+        // String str = sc.nextLine();
+        String s = "hello";
+        char ss = s.charAt(1);
+        System.out.println(ss);
+    }
+}
