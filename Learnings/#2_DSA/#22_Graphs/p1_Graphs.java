04599ea742c849413916615d489bd5fa4f4a7c18
adocxwork <adipracc@gmail.com>
2024-09-26 00:48:17 +0530

    files added..

---
+++ Learnings/#2_DSA/#22_Graphs/p1_Graphs.java
@@ -0,0 +1,18 @@
+import java.util.*;;
+public class p1_Graphs {
+    static class Edge {
+        int src;
+        int dest;
+        public Edge (int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+    public static void createGraph(ArrayList<Edge> graph[]) {
+        // c
+    }
+    public static void main(String[] args) {
+        int V = 4;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+    }
+}
