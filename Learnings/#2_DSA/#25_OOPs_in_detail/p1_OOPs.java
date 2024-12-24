8c6c3c86626b3f89218248972c00267a2b035c2f
adocxwork <adipracc@gmail.com>
2024-12-24 22:41:13 +0530

    files added..

---
+++ Learnings/#2_DSA/#25_OOPs_in_detail/p1_OOPs.java
@@ -0,0 +1,11 @@
+class Human {
+    int age;
+    String name;
+}
+public class p1_OOPs {
+    public static void main(String[] args) {
+        Human Aditya = new Human();
+        System.out.println(Aditya.age);
+        System.out.println(Aditya.name);
+    }
+}
